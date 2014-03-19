678014fcc6015ee1c97f2213e1ce67eda5597f3d
Pedro Côrte-Real <pedro@pedrocr.net>
2014-03-19 15:04:03 +0000

    Remove the old testrun file

--- bin/testrunsave.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require File.expand_path('../lib/camerasink', File.dirname(__FILE__))
-
-camera = CameraSink::Camera.new("file:///home/pedrocr/Projects/camerasink/bin/test.mkv", "testoutput/")
-camera.run
-$stderr.puts "Leaving so soon?"

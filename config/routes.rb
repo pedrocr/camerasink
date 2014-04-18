ee037f94aae59ba69262ef5e712bae0e8dc08cae
Pedro Côrte-Real <pedro@pedrocr.net>
2014-04-18 11:15:02 +0100

    Replace start controller with a real cameras one

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Camerasink::Application.routes.draw do
+  resources :cameras
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'start#index'
+  root to: redirect('/cameras')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
